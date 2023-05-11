aaa8eea962f213bc704d302d036cc5c1d216bc3d
Alok kumar prajapati <alokprajapati913@gmail.com>
2023-05-12 04:36:34 +0530

    Added: ifElse program

--- src/com/bridzelab/program/IfElse.java
+++ src/com/bridzelab/program/IfElse.java
@@ -1,4 +1,36 @@
 package com.bridzelab.program;
 
+import java.util.Scanner;
+
 public class IfElse {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter single Digit number 1 to 9");
+        int num=sc.nextInt();
+        if(num==1){
+            System.out.println("One");
+        }
+        else if(num==2){
+            System.out.println("Two");
+        }
+        else if(num==3){
+            System.out.println("Three");
+        }
+        else if(num==4){
+            System.out.println("Four");
+        }else if(num==5){
+            System.out.println("Five");
+        }else if(num==6){
+            System.out.println("Six");
+        }else if(num==7){
+            System.out.println("Seven");
+        }else if(num==8){
+            System.out.println("Eight");
+        }
+        else if(num==9){
+            System.out.println("Nine");
+        }else{
+            System.out.println("enter wrong value");
+        }
+    }
 }
